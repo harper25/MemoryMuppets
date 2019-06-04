0525b955397051ccea26dc9de8812291d73c3651
Kuba25 <olesjakubb@gmail.com>
2019-06-04 18:35:00 +0200

    Sounds from files again

--- memorymuppets/ui.py
+++ memorymuppets/ui.py
@@ -1,5 +1,5 @@
 from PySide2.QtMultimedia import QMediaPlayer
-from PySide2.QtCore import QSize, Qt, QUrl, Signal
+from PySide2.QtCore import QFileInfo, QSize, Qt, QUrl, Signal
 from PySide2.QtGui import QPixmap, QCursor
 from PySide2.QtWidgets import QWidget
 from PySide2.QtWidgets import QPushButton, QVBoxLayout, QGridLayout
@@ -75,8 +75,9 @@ def build(win):
         win.muppets.append(MuppetButton(f":/b{i+1}", f":/b{i+1}a", i))
         layout_buttons.addWidget(win.muppets[i], 0, i)
         # layout_buttons.setColumnMinimumWidth(i, 91)  # win 91, mac 98
+        path_to_sound = QFileInfo(f"sounds/s{i + 1}.wav").absoluteFilePath()
         win.muppets[i].sound = QMediaPlayer()
-        win.muppets[i].sound.setMedia(QUrl(f"qrc:/s{i+1}"))
+        win.muppets[i].sound.setMedia(QUrl.fromLocalFile(path_to_sound))
         win.muppets[i].signal.connect(win.muppet_pressed(i))
 
     # central widget
@@ -120,9 +121,11 @@ def build(win):
     win.levels_widget.hide()
 
     # sounds start & failure
+    path_to_sound = QFileInfo("sounds/start.wav").absoluteFilePath()
     win.start = QMediaPlayer()
-    win.start.setMedia(QUrl('qrc:/start'))
+    win.start.setMedia(QUrl.fromLocalFile(path_to_sound))
+    path_to_sound = QFileInfo("sounds/end.wav").absoluteFilePath()
     win.end = QMediaPlayer()
-    win.end.setMedia(QUrl('qrc:/end'))
+    win.end.setMedia(QUrl.fromLocalFile(path_to_sound))
 
     win.muppets[0].sound.play()
